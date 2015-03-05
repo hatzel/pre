f9784f0953ec5449c0476dacdbfb75b0b411936a
Hans Ole Hatzel <hhatzel@gmail.com>
2015-03-05 19:24:06 +0100

    Some more regex magic...

--- benchrec.sh
+++ benchrec.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-find . -type f -not -empty -exec echo {} \; -exec fsbench $@ {} \;
+echo "Used codec,version,args,E.time (ms),E. input size,E. output size,E. iters,D. time(ms),D. output size,D. iters"
+name=""
+find . -type f -not -empty -exec echo {} \; -exec fsbench $@ {} \; | sed "/^.*terations.*/d;/^Codec/d"
